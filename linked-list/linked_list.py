623309b4b8d484fc107000e39ebe841017883f21
alan-chala <alan.chala000@gmail.com>
2026-02-09 17:54:02 -0500

    feat(linked-list): implement prepend method

--- linked-list/linked_list.py
+++ linked-list/linked_list.py
@@ -3,3 +3,9 @@ from node import Node
 class LinkedList():
   def __init__(self):
     self.head = None
+  
+  def prepend(self, data):
+    new_node = Node(data)
+    new_node.next = self.head
+    self.head = new_node
+    
