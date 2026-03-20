d87f0dd73d472c3a7794dc836bccf3da4dc7e9a5
alan-chala <alan.chala000@gmail.com>
2026-03-20 14:12:44 -0500

    feat(stack): implement pop method for stack class

--- stack/stack.py
+++ stack/stack.py
@@ -14,6 +14,19 @@ class Stack:
       new_node.next = self.top
       self.top = new_node
       self.size += 1
+      
+    def pop(self):
+      if not self.top:
+        return None
+      
+      else:
+        popped_node = self.top
+        self.top = self.top.next
+        
+        popped_node.next = None
+        self.size -= 1
+        
+        return popped_node.value
     
     
 my_stack = Stack()
