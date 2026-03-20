587004f26ec5a6f6b591c9c553aa9bb4e6a0cde8
alan-chala <alan.chala000@gmail.com>
2026-03-20 14:11:16 -0500

    feat(stack): implement push method for stack class

--- stack/stack.py
+++ stack/stack.py
@@ -5,5 +5,15 @@ class Stack:
     self.top = None
     self.size = 1
     
+  def push(self, data):
+    new_node = Node(data)
+
+    if not self.top:
+      self.top = new_node
+    else:
+      new_node.next = self.top
+      self.top = new_node
+      self.size += 1
+    
     
 my_stack = Stack()
