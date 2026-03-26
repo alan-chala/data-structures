d7c284d453a99e9759a56f6e3cae3dda07629630
alan-chala <alan.chala000@gmail.com>
2026-03-26 13:32:55 -0500

    feat(linked-list): implement reverseList function to reverse a linked list

---
+++ linked-list/exercises/reverse_linked_list.py
@@ -0,0 +1,11 @@
+def reverseList(head):
+  current = head
+  prev = None
+
+  while current:
+    temp = current.next
+    current.next = prev
+    prev = current
+    current = temp
+
+  return prev
